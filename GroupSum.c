bcb262a33a173a6a9eae72e0169926d088fc967b
Vinod Bera <91298867+vinodbera@users.noreply.github.com>
2023-07-24 08:49:02 +0530

    10

--- GroupSum.c
+++ GroupSum.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
-    int k;
-    scanf("%d", &k);
-    int i;
-    int sum = 0;
+    long long k;
+    scanf("%lld", &k);
+    long long i;
+    long long sum = 0;
     for(i=((k*k)+(k-1));i>((k-1)*(k-1)+(k-2));i--){
         if(i%2!=0){
             sum+=i;
         }
         
     }
-    printf("%d",sum);
+    printf("%lld",sum);
 }
