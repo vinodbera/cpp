643d38d1597f5250ae7e8e7663bd61701a6d8485
Vinod Bera <91298867+vinodbera@users.noreply.github.com>
2023-08-22 21:43:39 +0530

    44

---
+++ MatrixAddition.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(void)
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int A[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    scanf("%d %d", &n, &m);
+    int B[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    int C[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            C[i][j] = A[i][j] + B[i][j];
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
